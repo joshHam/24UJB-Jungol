0de1520204c051a342c854e6cb0e7c33c517ddaf
macbook <macbook@192.168.0.7>
2024-04-12 07:58:09 +0900

    s

---
+++ UJB24_Jungol_Home/src/q151/Main.java
@@ -0,0 +1,19 @@
+package q151;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int[]i= new int[5];
+		
+		for(int j=0;j<i.length;j++) {
+			i[j]=sc.nextInt();
+		}sc.close();
+		
+		
+		System.out.print(i[0]+i[2]+i[4]);
+	}
+
+}
