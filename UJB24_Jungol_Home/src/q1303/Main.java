54808347d015f134750954475555426f708887eb
joshHam <163796630+joshHam@users.noreply.github.com>
2024-05-22 14:37:18 +0900

    q1303

--- UJB24_Jungol_Home/src/q1303/Main.java
+++ UJB24_Jungol_Home/src/q1303/Main.java
@@ -14,10 +14,11 @@ public class Main {
 
 		int k = 1;
 
-		for (int i = 1; i <= n; i++) {
-			for (int j = 1; j <= m; j++) {
-				System.out.print(k + " ");
-				k++;
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < m; j++) {
+//				System.out.print(k + " ");
+//				k++;
+				System.out.print(k++ + " ");
 			}
 			System.out.println();
 		}
