d4aef5100cd51ff55ca94563ecf9b04a5d86cf68
joshHam <163796630+joshHam@users.noreply.github.com>
2024-05-29 12:51:59 +0900

    577

--- UJB24_Jungol_Home/src/q576/Main.java
+++ UJB24_Jungol_Home/src/q576/Main.java
@@ -1,10 +1,37 @@
 package q576;
 
-public class Main {
+import java.util.Scanner;
 
+public class Main {//통과안됨
+
+	public int Cal (int A, char S, int b) {
+		int res = 0;
+		
+		if(S!='+' && S!='-' && S!='*' && S!='/') {
+			return res;
+		}else {
+//			res = (int)A+Integer.toString(S)(int)b;
+			
+			
+			return (A+S+b);
+		}
+		
+		
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int A = sc.nextInt();
+		String S = sc.next();
+		int b = sc.nextInt();
+		sc.close();
+		
+		Main m;/* 레퍼런스변수 선언 */
+		m = new Main();/* 인스턴스 생성 */
 
+		System.out.println(m.Cal(A, S.charAt(0), b));
+		
 	}
 
 }
---
+++ UJB24_Jungol_Home/src/q577/Main.java
@@ -0,0 +1,43 @@
+package q577;
+
+import java.util.Scanner;//김성윤풀이
+
+
+class Mat {
+   public int getD() {
+      return D;
+   }
+   public void setD(int d) {
+      D = d;
+   }
+   public int getE() {
+      return E;
+   }
+   public void setE(int e) {
+      E = e;
+   }
+   int D ;
+   int E;
+   
+}
+public class Main {
+
+   public static void main(String[] args) {
+      Scanner sc = new Scanner(System.in);
+      int A = sc.nextInt();
+      int B = sc.nextInt();
+      Mat mat = new Mat();
+      mat.setD(A);
+      mat.setE(B);
+      
+      if (mat.getD()>mat.getE()) {
+         System.out.print(mat.getD()/2+ " ");
+         System.out.print(mat.getE()*2);
+      }
+      else if (mat.getD()<mat.getE()) {
+         System.out.print(mat.getD()*2+ " ");
+         System.out.print(mat.getE()/2+ " ");
+      }
+   }
+
+}
