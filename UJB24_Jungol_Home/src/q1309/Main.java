dad4301d58aa524f3c47ad34dfd84250f7c895ea
joshHam <163796630+joshHam@users.noreply.github.com>
2024-06-04 17:46:15 +0900

    q1309

--- UJB24_Jungol_Home/src/q1309/Main.java
+++ UJB24_Jungol_Home/src/q1309/Main.java
@@ -2,17 +2,32 @@ package q1309;
 
 import java.util.Scanner;
 
-public class Main {	//DP(memoization)방법풀이	//재귀방법풀이도찾기
+public class Main {	
+	
+	//재귀방법풀이
+	public static long f(long p) {
+		if(p == 1) {
+			System.out.println("1! = 1");
+			return 1;
+		}else {
+			System.out.println(p + "! = "+p+" * "+(p-1)+"!");
+			return p*f(p-1);
+		}
+	}
+	
 
+	
+	
+	//내풀이	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
+		long n = sc.nextLong();
 		sc.close();
 		
 		long res = 1;
 		
-		for(int i=n;i>0;i--) {
+		for(long i=n;i>0;i--) {
 			if(i>1) {
 			System.out.printf("%d! = %d * %d! %n", i, i, i-1);
 			}else {
@@ -22,6 +37,11 @@ public class Main {	//DP(memoization)방법풀이	//재귀방법풀이도찾기
 				
 		}
 		System.out.println(res);
+		System.out.println();
+		
+		//재귀방법정답///////////////////////////////////////
+		long ret = f(n);
+		System.out.println(ret);
 		
 	}
 
---
+++ UJB24_Jungol_Home/src/q1309/Main2.java
@@ -0,0 +1,33 @@
+package q1309;
+
+import java.util.Scanner;
+						//박현옥풀이
+public class Main2 {//DP(memoization)방법풀이	
+
+   public static void main(String args[]) {
+      Scanner sc = new Scanner(System.in);
+      int n = sc.nextInt();
+      long fact[] = new long[n + 1];
+      if (n == 0) {
+         System.out.println(n + "!" + " = " + 1);
+      } else {
+         fact[0] = 1;
+         fact[1] = 1;
+         for (int i = 1; i <= n; i++) {
+            fact[i] = i * fact[i - 1];
+         }
+
+         for (int i = n; i >= 1; i--) {
+            System.out.print(i + "!" + " = ");
+            if (i == 1) {
+               System.out.println(i);
+            } else {
+               System.out.println(i + " * " + (i - 1) + "!");
+            }
+         }
+      }
+      sc.close();
+      System.out.println(fact[n]);
+
+   }
+}
